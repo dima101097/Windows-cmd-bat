11dd95ff60af36e7dfe34ee83def53bb74888544
dima101097 <cef101097@gmail.com>
2021-09-28 11:35:58 +0300

    add script

---
+++ PrinterFIX.cmd
@@ -0,0 +1,31 @@
+@echo off
+Title Local Printer FIX
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+  
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:starts 
+
+echo Add reg parametr
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Print" /v "RpcAuthnLevelPrivacyEnabled" /t REG_DWORD /d "0"
+echo Done
+net stop "spooler"
+net start "spooler"
+echo FINISH!
+timeout /t 5 
