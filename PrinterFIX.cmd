5190c34aa8da83917c02b43b7877b899f66bc207
dima101097 <cef101097@gmail.com>
2021-11-02 13:54:38 +0200

    add coment

--- PrinterFIX.cmd
+++ PrinterFIX.cmd
@@ -1,21 +1,17 @@
+rem Instal on printer-server PC !!!!
+rem Fix eror 0x0000011b for Windows 10
+
 @echo off
 Title Local Printer FIX
-REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     goto UACPrompt
 ) else ( goto gotAdmin )
-
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
     "%temp%\getadmin.vbs"
     exit /B
-  
-
 :gotAdmin
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
     pushd "%CD%"
