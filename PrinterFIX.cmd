9f8603f659f36d4b2536af5d35b7f293c263d7a9
dima101097 <cef101097@gmail.com>
2021-12-10 08:54:03 +0200

    New metod enhancements of rights

--- PrinterFIX.cmd
+++ PrinterFIX.cmd
@@ -3,21 +3,9 @@ rem Fix eror 0x0000011b for Windows 10
 
 @echo off
 Title Local Printer FIX
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-:starts 
-
+cls
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)    
+cls
 echo Add reg parametr
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Print" /v "RpcAuthnLevelPrivacyEnabled" /t REG_DWORD /d "0"
 echo Done
